4d1134bb733d80539ecce5664a65bef64a4821f8
Benja-Boi <75277557+Benja-Boi@users.noreply.github.com>
2021-05-21 17:08:31 +0300

    Ophir Update #12 - Toggle UI
    -Added character toggle UI with animations

---
+++ Assets/ToggleAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAnimation : MonoBehaviour
+{
+    Animator animator;
+    GameManager gm;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("Light Active", gm.whiteActive);
+    }
+}
