9d9125dcb155862d889c195440ed25126c485364
GolanAmir <74215609+GolanAmir@users.noreply.github.com>
2021-06-07 10:08:44 +0300

    Golan's Agents
    AI mechanics

---
+++ Assets/AIMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("White Hand").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    
+    void Update()
+    {
+        if (GetComponent<FollowHolder>().pickedUp)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
