86d8ed3b1d623ac164600a63d28677545e56c893
Benja-Boi <75277557+Benja-Boi@users.noreply.github.com>
2021-06-11 18:01:53 +0300

    Ophir Update #19
    - Toggle issues fixed on all maps (TEST)
    - Toggle button no longer shows on pause and end screen
    - End lv and Pause menus fit screen size and:
    added color and audio feedback to buttons
    - Fit placement tilemap to hard lv 1
    - Repositioned "back" button on levels scene

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleButton : MonoBehaviour
+{
+
+    private Animator anim;
+    private GameManager gm;
+
+    void Start()
+    {
+        this.gameObject.SetActive(true);
+        anim = GetComponent<Animator>();
+        gm = FindObjectOfType<GameManager>();
+
+    }
+
+    
+    void Update()
+    {
+        anim.SetBool("Light Active", gm.whiteActive);
+        anim.SetBool("Swap Enabled", gm.swapEnabled);
+        if (gm.levelPassed)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/disableToggle.cs
+++ Assets/disableToggle.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 
 public class disableToggle : MonoBehaviour
 {
-    public bool toggleOn;
-    public GameObject toggleBar;
-    // Update is called once per frame
-    void Update()
-    {
 
-        if (toggleOn)
-        {
-            toggleBar.SetActive(true);
-        }
-        else
-        {
-            toggleBar.SetActive(false);
-        }
-    }
 }
